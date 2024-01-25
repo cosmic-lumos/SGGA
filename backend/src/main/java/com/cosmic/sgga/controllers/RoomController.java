551dfd0a39bbe8092f2e8575cc04832b2daea5ed
BiteSnail <rkwoal216@gmail.com>
2024-01-25 15:30:49 +0900

    feat: room만들기 기능 구현

--- backend/src/main/java/com/cosmic/sgga/controllers/RoomController.java
+++ backend/src/main/java/com/cosmic/sgga/controllers/RoomController.java
@@ -1,8 +1,18 @@
 package com.cosmic.sgga.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cosmic.sgga.dtos.RoomDto;
+import com.cosmic.sgga.dtos.RoomMakeDto;
+import com.cosmic.sgga.entities.Room;
+import com.cosmic.sgga.services.RoomService;
+
+import jakarta.transaction.Transactional;
 
 /**
  * 방에 대한 api를 처리하는 컨트롤러
@@ -14,5 +24,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path="/api")
 public class RoomController {
+    @Autowired
+    private RoomService roomService;
+
+    @Transactional
+    @PostMapping("/rooms")
+    public @ResponseBody RoomDto createRoom(@RequestBody RoomMakeDto roomInfo) {
+        Room newRoom = roomService.createRoom(roomInfo);
+        
+        return RoomDto.toDTO(newRoom);
+    }
+    
 
 }
