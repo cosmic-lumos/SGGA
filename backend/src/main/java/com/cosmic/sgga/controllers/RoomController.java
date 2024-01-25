268237887199adeda855c5d7adbd7136088bb315
BiteSnail <rkwoal216@gmail.com>
2024-01-25 14:40:00 +0900

    feat: 룸 컨트롤러 생성

---
+++ backend/src/main/java/com/cosmic/sgga/controllers/RoomController.java
@@ -0,0 +1,18 @@
+package com.cosmic.sgga.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 방에 대한 api를 처리하는 컨트롤러
+ * 
+ * @author rkwoal216@gmail.com
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping(path="/api")
+public class RoomController {
+
+}
