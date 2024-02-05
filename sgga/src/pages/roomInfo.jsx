7afc5893bd8af55989e259c84089199d2e4b96c2
BiteSnail <rkwoal216@gmail.com>
2024-02-05 11:37:14 +0900

    feat: 연결 전 피젯스피너 추가

--- sgga/src/pages/roomInfo.jsx
+++ sgga/src/pages/roomInfo.jsx
@@ -1,14 +1,25 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { StompSessionProvider, useSubscription, useStompClient } from "react-stomp-hooks";
+import { Container, Row, Col, Form, Button, Spinner } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const RoomInfo = (props) => {
-    const { code }=useParams();
+    const [spinnerMode, setSpinnerMode] = useState("border");
+    const { code } = useParams();
 
-    return (       
+    return (
         <StompSessionProvider
+            onConnect={()=>{
+                setSpinnerMode("");
+            }}
             url={'ws://localhost:8080/sgga-websocket'}>
-            <Information code={code}/>
+                <Container>
+                    <Spinner animation={spinnerMode} role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                    <Information code={code} hidden={spinnerMode === "border"}/>
+                </Container>
         </StompSessionProvider>
     )
 }
@@ -19,26 +30,26 @@ const Information = (props) => {
     const stompClient = useStompClient();
 
     const sendMessage = () => {
-        if(stompClient){
+        if (stompClient) {
             stompClient.publish({
                 destination: `/app/${props.code}/join`,
-                body: JSON.stringify({'name': "꽃뱀"})
+                body: JSON.stringify({ 'name': "꽃뱀" })
             })
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log(info);
     }, [info])
 
-    useSubscription("/room/"+props.code, (roomDto) => {
+    useSubscription("/room/" + props.code, (roomDto) => {
         setInfo(JSON.parse(roomDto.body));
     });
     return (
-        <div>
+        <Row hidden={props.hidden}>
             {props.code}
             <button onClick={sendMessage}>눌러봐</button>
-        </div>
+        </Row>
     )
 }
 
