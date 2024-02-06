a8bb539ce7529cd3eb87b29849a0281fe5a8f608
이동율 <dy9623@naver.com>
2024-02-06 14:27:20 +0900

    feat: 방만들기 페이지 추가중

--- sgga/src/pages/createRoomPage.js
+++ sgga/src/pages/createRoomPage.js
@@ -1,7 +1,21 @@
 import "../styles/createRoomPage.css";
 
 function CreateRoomPage() {
-  return <div></div>;
+  return (
+    <div className="createRoomPage">
+      <div className="title">SGGA</div>
+      <div className="buttonSet">
+        <div className="fourPersonTable">
+          <div className="text">4인용 테이블 개수</div>
+          <div className="modify">
+            <div className="modifyButton">-</div>
+            <div className="fourtablenum">수량</div>
+            <div className="modifyButton">+</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateRoomPage;
