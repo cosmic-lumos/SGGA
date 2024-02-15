67d4856c623277860b618c94ff72a04a3758b0aa
이동율 <dy9623@naver.com>
2024-02-15 14:31:59 +0900

    feat: roomPage css 수정

--- sgga/src/pages/roomPage.jsx
+++ sgga/src/pages/roomPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/roomPage.css";
 
 const RoomPage = () => {
   const location = useLocation();
@@ -22,15 +23,18 @@ const RoomPage = () => {
       setValidated(true);
       return;
     }
-    navigate(`/room/${code}`, {state: {...userInfo}});
+    navigate(`/room/${code}`, { state: { ...userInfo } });
   };
 
   return (
-    <Container>
+    <Container className="h-100 d-flex align-items-center justify-content-center">
       <Row>
         <div>
           <Form noValidate validated={validated} onSubmit={onClickSubmit}>
-            <Form.Label htmlFor="roomCode">방 입장코드</Form.Label>
+            <Form.Label htmlFor="roomCode">
+              <div className="enterMessage">방 입장코드</div>
+              <br></br>
+            </Form.Label>
             <Form.Control
               type="text"
               id="roomCode"
@@ -38,11 +42,14 @@ const RoomPage = () => {
               required
               onChange={onChangeRandomCode}
             />
+            <br></br>
             <Form.Control.Feedback type="invalid">
-              입장코드를 입력해 주세요.
+              <div className="enterMessage">입장코드를 입력해 주세요.</div>
             </Form.Control.Feedback>
             <Form.Text as="p" id="roomCodeHelpBlock" muted>
-              방 생성시 주어지는 랜덤 코드를 입력하세요.
+              <div className="enterMessage">
+                방 생성시 주어지는 랜덤 코드를 입력하세요.
+              </div>
             </Form.Text>
             <Button type="submit" variant="primary">
               입장
